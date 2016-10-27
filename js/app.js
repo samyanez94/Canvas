1b0c5227a4a5973fd94acaad28ba4c7a9b8ca1b3
Samuel Yanez <samuelyanez94@gmail.com>
2016-10-26 22:33:03 -0400

    Added comments and changes to the CSS

--- js/app.js
+++ js/app.js
@@ -1,83 +1,82 @@
-var color = $(".selected").css("background-color");
-var $canvas = $("canvas");
-var canvas = $("canvas")[0];
-var context = canvas.getContext("2d");
-var lastEvent;
-var mouseDown = false;
-var $saveImage = $('#saveImage');
+//Selects the canvas element
+var canvas = $("canvas");
 
-//When clicking on control list items
-$(".colors").on("click", "li", function () {
-    //Deselect sibling elements
-    $(this).siblings().removeClass("selected");
-    //Select clicked element
-    $(this).addClass("selected");
-    //Cache current color
-    color = $(this).css("background-color");
-});
+//Checks for browser support.
+if (canvas[0].getContext) {
 
-//When "New Color" is pressed
-$("#revealColorSelect").click(function () {
-    //Show color select or hide the color select
-    changeColor();
-    $("#colorSelect").toggle();
-});
+    $(".controls").toggle();
+    $(".colors").toggle();
 
-//update the new color span
-function changeColor() {
-    var r = $("#red").val();
-    var g = $("#green").val();
-    var b = $("#blue").val();
-    $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
-}
+    //Access the rendering context of the canvas
+    var context = canvas[0].getContext("2d");
+    //Selected color
+    var color = $(".selected").css("background-color");
+    //Last mouse event
+    var lastEvent;
+    //The mouse is up by defult
+    var mouseDown = false;
 
-//When color sliders change
-$("input[type=range]").change(changeColor);
+    //Changes the current color when clicking on control list items
+    $(".colors").on("click", "li", function () {
+        $(this).siblings().removeClass("selected");
+        $(this).addClass("selected");
+        color = $(this).css("background-color");
+    });
 
-//When "Add Color" is pressed
-$("#addNewColor").click(function () {
-    //Append the color to the colors ul
-    var $newColor = $("<li></li>");
-    $newColor.css("background-color", $("#newColor").css("background-color"));
-    $(".colors ul").append($newColor);
-    //Select the new color
-    $newColor.click();
-});
+    //Reveals the color picker when "New Color" is pressed
+    $("#revealColorSelect").click(function () {
+        changeColor();
+        $("#colorSelect").toggle();
+    });
 
-//On mouse events on the canvas
-$canvas.mousedown(function (e) {
-    lastEvent = e;
-    mouseDown = true;
-}).mousemove(function (e) {
-    //Draw lines
-    if (mouseDown) {
-        context.beginPath();
-        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-        context.lineTo(e.offsetX, e.offsetY);
-        context.strokeStyle = color;
-        context.stroke();
-        lastEvent = e;
+    //Updates the new color
+    function changeColor() {
+        var r = $("#red").val();
+        var g = $("#green").val();
+        var b = $("#blue").val();
+        $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
     }
-}).mouseup(function () {
-    mouseDown = false;
-}).mouseleave(function () {
-    $canvas.mouseup();
-});
 
-//When reset button is clicked....
-$("#reset").click(function () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-});
+    //Listens to when sliders change and calls the changeColor method
+    $("input[type=range]").change(changeColor);
 
-// Saving the canvas into an image
-var $snapshots = $('#snapshots');
-$saveImage.click(function () {
-    // Create a new image
-    var image = new Image();
-    // Set the image source to the canvas data URL
-    image.src = canvas.toDataURL();
-    // Append the snapshot to the div
-    $snapshots.append(image);
-    // Disble the 'save image' button
-    $saveImage.prop('disabled', true);
-});
+    //Adds the new color to the list when "Add Color" is pressed
+    $("#addNewColor").click(function () {
+        var newColor = $("<li></li>");
+        newColor.css("background-color", $("#newColor").css("background-color"));
+        $(".colors ul").append(newColor);
+        newColor.click();
+    });
+
+    //Mouse events on the canvas listeners
+    canvas.mousedown(function (e) {
+        lastEvent = e;
+        mouseDown = true;
+    }).mousemove(function (e) {
+        //Listens to when the mouse moves and draws a line from the point where the mouse first touched the canvas
+        if (mouseDown) {
+            context.beginPath();
+            context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+            context.lineTo(e.offsetX, e.offsetY);
+            context.strokeStyle = color;
+            context.stroke();
+            lastEvent = e;
+        }
+    }).mouseup(function () {
+        mouseDown = false;
+    }).mouseleave(function () {
+        //If the mouse leaves the canvas, is the same as lifting a pencil
+        canvas.mouseup();
+    });
+
+    //Clears the context of the canvas when the "Reset" button is clicked
+    $("#reset").click(function () {
+        context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+    });
+
+    //Downloads the content of the canvas
+    $("#save").click(function () {
+        $("#save")[0].href = canvas[0].toDataURL();
+        $("#save")[0].download = "image.png";
+    });
+}
